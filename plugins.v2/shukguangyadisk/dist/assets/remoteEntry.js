0d701cf3d94496dfade11fcae7cd9254386ee23e
liheng-lk <69590718+liheng-lk@users.noreply.github.com>
2026-08-10 14:25:35 +0800

    release: switch Page entry to v2.2.16 wrapper

--- plugins.v2/shukguangyadisk/dist/assets/remoteEntry.js
+++ plugins.v2/shukguangyadisk/dist/assets/remoteEntry.js
@@ -2,7 +2,7 @@ import { dynamicLoadingCss, get as legacyGet, init } from './remoteEntry_legacy.
 
 const get = (module) => {
   if (module === './Page') {
-    return import('./__federation_expose_AssistantPage-dev.js').then((mod) => () => mod.default);
+    return import('./__federation_expose_AssistantPage-216.js').then((mod) => () => mod.default);
   }
   if (module === './Config') {
     return import('./__federation_expose_AssistantConfig-dev.js').then((mod) => () => mod.default);
