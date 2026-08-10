9472748b623825b328ddb19d2c578ca3deafb41b
liheng-lk <69590718+liheng-lk@users.noreply.github.com>
2026-08-10 09:41:28 +0800

    fix: load cache-busted 2.2.6 login frontend

--- plugins.v2/shukguangyadisk/dist/assets/remoteEntry.js
+++ plugins.v2/shukguangyadisk/dist/assets/remoteEntry.js
@@ -3,7 +3,7 @@ import { dynamicLoadingCss, get as legacyGet, init } from './remoteEntry_legacy.
 const get = (module) => {
   if (module === './Page') {
     dynamicLoadingCss(['Page-76c96a1e.css'], false, './Page');
-    return import('./__federation_expose_DualLoginPage.js').then((mod) => () => mod.default);
+    return import('./__federation_expose_DualLoginPage-226.js').then((mod) => () => mod.default);
   }
   return legacyGet(module);
 };
