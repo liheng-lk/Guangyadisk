8c0eb86db65fadb20dea93deec509288038902ab
liheng-lk <69590718+liheng-lk@users.noreply.github.com>
2026-08-10 12:47:48 +0800

    feat(ui): switch plugin to Guangya Cloud Assistant page

--- plugins.v2/shukguangyadisk/dist/assets/remoteEntry.js
+++ plugins.v2/shukguangyadisk/dist/assets/remoteEntry.js
@@ -2,8 +2,8 @@ import { dynamicLoadingCss, get as legacyGet, init } from './remoteEntry_legacy.
 
 const get = (module) => {
   if (module === './Page') {
-    dynamicLoadingCss(['Page-213.css'], false, './Page');
-    return import('./__federation_expose_DualLoginPage-213.js').then((mod) => () => mod.default);
+    dynamicLoadingCss(['Page-76c96a1e.css'], false, './Page');
+    return import('./__federation_expose_AssistantPage-214.js').then((mod) => () => mod.default);
   }
   return legacyGet(module);
 };
